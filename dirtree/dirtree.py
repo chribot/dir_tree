d0bc2fd561efd1abfb98bb1146b6a73c47486dea
chribot <chribot.git@gmail.com>
2022-03-25 11:31:51 +0100

    check read permissions, don't follow symlinks

--- dirtree/dirtree.py
+++ dirtree/dirtree.py
@@ -14,13 +14,10 @@ def print_dir(dir_name: str, lines_before: str, is_last: bool = False):
     else:       print( lines_before + '├──', dir_name + '/' )
 
 def print_tree( dir_dict: dict, 
-                    lines_before: str = None, 
-                    is_last_dir: bool = False):
+                lines_before: str = None, 
+                is_last_dir: bool = False ):
     # depth of dirtree
-    depth = 0
-    if lines_before != None:
-        if lines_before == '': depth = 1
-        else: depth = len(lines_before)//2 + 1
+    depth = 0 if lines_before == None else len(lines_before)//2 + 1
     
     # print current directory
     if depth == 0:
@@ -29,92 +26,97 @@ def print_tree( dir_dict: dict,
     else:
         print_dir(dir_dict['dir_name'], lines_before, is_last_dir)
 
-    count_dirs = len(dir_dict['dir_list'])      # dirs in current dir
-    count_files = len(dir_dict['file_list'])    # files in current dir
+    count_dirs = len(dir_dict['dir_list'])     # dirs in current dir
+    count_files = len(dir_dict['file_list'])   # files in current dir
 
     # print subdirs
     for i, d in enumerate(dir_dict['dir_list']):
         line = ''
         if (i == count_dirs-1) & (count_files == 0):
-            d_is_last = True                    # subdirectory has no files
+            d_is_last = True                   # last subdirectory has no files
         else: d_is_last = False
         if depth >= 1:
-            if is_last_dir: line += '  '        # parent directory has no files
-            else: line += '│ '                  # parent directory has files
-        print_tree(d, lines_before + line, d_is_last)
+            if is_last_dir: line += '  '       # parent directory has no files
+            else: line += '│ '                 # parent directory has files
+        print_tree(d, lines_before + line, d_is_last) # recursion
 
     # print files
     for i, d in enumerate(dir_dict['file_list']):
         line = ''
         if depth >= 1:
-            if is_last_dir: line += '  '        # parent directory has no files
-            else: line += '│ '                  # parent directory has files
+            if is_last_dir: line += '  '       # parent directory has no files
+            else: line += '│ '                 # parent directory has files
         if (i == count_files-1): 
-            is_last_file = True                 # last file in directory
+            is_last_file = True                # last file in directory
         else: is_last_file = False
         print_file(d, lines_before + line, is_last_file)
 
 def get_dir_dict(path: str) -> dict:
     """Returns a Directory-Tree of a given Directory (path) as Dictionary."""
     if path == '': path = os.getcwd()
+    else: path = os.path.abspath(path)
+    if path[-1] != '/': path += '/'
+
     dict_dir = {}
     with os.scandir(path) as scan:
         files = []
         dir_names = []
         for d in scan:
             if (d.name[0] == '.') & ignore_dot_files:
-                break
+                pass
             else:
-                if d.is_dir(): dir_names.append(d.name)
+                if d.is_dir(follow_symlinks=False): 
+                    dir_names.append(d.name)
                 else: files.append(d.name)
         files.sort()
-    scan.close()
 
-    dict_dir['path'] = '/'.join( path.split('/')[:-1] )
-    dict_dir['dir_name'] = path.split('/')[-1]
-    dict_dir['file_list'] = files
+    if path == '/': dir_name = ''
+    else: dir_name = path.split('/')[-2]
+
     dir_list = []
     if dir_names:
-        # nach Kleinbuchstaben sortieren, ansonsten kommt 'Gb' vor 'ga'
-        dir_names.sort(key=str.lower)
+        dir_names.sort(key=str.lower)                     # use lower case sort
         for dn in dir_names:
-            dir_list.append( get_dir_dict( path + '/' + dn ) )      # Rekursion
+            if os.access(path + dn, os.R_OK):           # check read permission
+                dir_list.append( get_dir_dict( path + dn ) )        # recursion
+
+    dict_dir['path'] = path
+    dict_dir['dir_name'] = dir_name
+    dict_dir['file_list'] = files
     dict_dir['dir_list'] = dir_list
 
     return dict_dir
         
 
-#
 # MAIN ------------------------------------------------------------------------
-#
+if __name__ == "__main__":
 
-# data structure for dir_dict:
-'''
-{
-    'path' : '/path'
-    'dir_name': 'dir_tree_project',
-    'file_list' : ['README.md', 'tree.py'],
-    'dir_list' : [
-        {
-            'path' : '/path/dir_tree_project'
-            'dir_name' : 'dirtree',
-            'file_list' : ['dirtree.py', '__init__.py', 'cli.py'],
-            'dir_list' : []
-        },
-        { ... },
-        { ... }
-    ]
-}
-'''
+    # data structure for dir_dict:
+    '''
+    {
+        'path' : '/path'
+        'dir_name': 'dir_tree_project',
+        'file_list' : ['README.md', 'tree.py'],
+        'dir_list' : [
+            {
+                'path' : '/path/dir_tree_project'
+                'dir_name' : 'dirtree',
+                'file_list' : ['dirtree.py', '__init__.py', 'cli.py'],
+                'dir_list' : []
+            },
+            { ... },
+            { ... }
+        ]
+    }
+    '''
 
-ignore_dot_files = True
+    ignore_dot_files = True
 
-print()
-path = os.getcwd()
-#path = '..'
-#path = os.path.abspath(path)
-#path = os.path.relpath(path)
+    print()
+    path = os.getcwd()
+    #path = '..'
+    #path = '/'
 
-dict_dir = get_dir_dict(path)
-print_tree(dict_dir)
-print()
+    dict_dir = get_dir_dict(path)
+    print_tree(dict_dir)
+    print()
